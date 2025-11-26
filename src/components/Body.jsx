48afb451419f24e865a6899b4a0244726b373361
Gaurav Tomar <gauravtomar977@gmail.com>
2025-11-27 00:25:06 +0530

    check user authenticate or not if not then redirect login page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((store)=> store.user);
+  const navigate = useNavigate();
+
+  const fetchData = async ()=>{
+    try {
+      if (userData) return;
+      const res = await axios.get(BASE_URL +"/profile/view",{withCredentials: true});
+      dispatch(addUser(res.data));
+     
+    } catch (err) {
+      if(err.status === 400){
+        navigate("/login");
+      }
+      console.error(err);
+    } 
+  }
+  useEffect(()=>{
+    fetchData();
+    },[]);
   return (
     <div>
       <Navbar/>

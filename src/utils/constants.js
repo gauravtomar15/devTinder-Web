3cc12b2d9671b13d6fd4659958cd3cb62863b72a
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-09 00:45:28 +0530

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:7777"
-    : "https://devtinder-backend-2j08.onrender.com/api";
+    : "https://devtinder-backend-2j08.onrender.com";
 
 export const getProfileImageUrl = (photoUrl) => {
   if (!photoUrl) return "https://geographyandyou.com/images/user-profile.png";

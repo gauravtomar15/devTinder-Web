b68192c0ef9e68c1d8fcb7999e5de418cf5ff9a1
Gaurav Tomar <gauravtomar977@gmail.com>
2026-08-09 01:07:14 +0530

    change baseurl

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,12 +2,7 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("https://gauravdevsocial.site", {
-      path: "/api/socket.io",
-      withCredentials: true,
-    });
-  }
+  return io(BASE_URL, {
+    withCredentials: true,
+  });
 };
